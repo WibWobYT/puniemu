421c0449b75bbf52d0e87adc2b4a86e41538c187
WibWobYT <129542629+WibWobYT@users.noreply.github.com>
2026-07-27 22:57:05 +0200

    Update YwpUserGachaEntry.cs

--- Src/Server/GameServer/Requests/ExecuteGacha/DataClasses/YwpUserGachaEntry.cs
+++ Src/Server/GameServer/Requests/ExecuteGacha/DataClasses/YwpUserGachaEntry.cs
@@ -1,15 +1,1 @@
-﻿using Newtonsoft.Json;
 
-// CREATED BY WIBWOB_YT
-
-namespace Puniemu.Src.Server.GameServer.DataClasses
-{
-    public class YwpUserGachaEntry
-    {
-        [JsonProperty("feverPctg")]
-        public int FeverPctg { get; set; }
-
-        [JsonProperty("gachaType")]
-        public int GachaType { get; set; }
-    }
-}
